77f87f982d3dd96f19cb34c8d6ac348898efe434
Marc Dressler <madmac.123456789@gmail.com>
2024-03-27 21:13:25 +0100

    Baum42Blau

---
+++ L01_Zufallsgedicht/scriptZufafallsgedicht.js
@@ -0,0 +1,14 @@
+let subjekte = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"]
+let preadikate = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"]
+let objekte = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"]
+
+console.log(subjekte, preadikate, objekte);
+
+for (let index = 6; index > 0; index--) {
+    console.log(index);
+    
+}
+
+function getVerse(subjekte,preadikate,objekte) {
+    
+}
