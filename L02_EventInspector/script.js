381b18b00f8ee4bdd0bf9d7f46ea30e34cc99c68
Marc Dressler <madmac.123456789@gmail.com>
2024-04-11 14:31:37 +0200

    counter Null error to be fixed

--- L02_EventInspector/script.js
+++ L02_EventInspector/script.js
@@ -1,2 +1,13 @@
 "use strict";
+let counter = 1;
+const increase = document.getElementById("increase");
+const decrease = document.getElementById("decrease");
+increase.addEventListener("click", () => {
+    counter++;
+    console.log(counter);
+});
+decrease.addEventListener("click", () => {
+    counter--;
+    console.log(counter);
+});
 //# sourceMappingURL=script.js.map
---
+++ L02_EventInspector/script.ts
@@ -0,0 +1,14 @@
+let counter =1;
+const increase = document.getElementById("increase") as HTMLButtonElement
+const decrease = document.getElementById("decrease") as HTMLButtonElement
+
+increase.addEventListener("click",() =>{
+    counter++;
+    console.log(counter);
+    
+});
+decrease.addEventListener("click",() =>{
+    counter--;
+    console.log(counter);
+    
+});
