64d2170d206035568fd170c85540eda2b0885faf
Marc Dressler <madmac.123456789@gmail.com>
2024-04-11 23:19:00 +0200

    try somestuff On branch try

--- L02_EventInspector/script.js
+++ L02_EventInspector/script.js
@@ -2,6 +2,17 @@
 var counter = 0;
 const increase = document.getElementById("increase");
 const decrease = document.getElementById("decrease");
+document.addEventListener('DOMContentLoaded', () => {
+    const cursor = document.querySelector('cursor');
+    const positionDisplay = document.getElementById('position');
+    document.addEventListener("mousemove", (e) => {
+        const x = e.clientX;
+        const y = e.clientY;
+        cursor.style.left = x + "px";
+        cursor.style.top = y + "px";
+        positionDisplay.textContent = `${e.pageX}px, ${e.pageY}px`;
+    });
+});
 increase.addEventListener("click", () => {
     counter++;
     console.log(counter);
--- L02_EventInspector/script.ts
+++ L02_EventInspector/script.ts
@@ -2,6 +2,19 @@ var counter =0;
 const increase = document.getElementById("increase") as HTMLButtonElement
 const decrease = document.getElementById("decrease") as HTMLButtonElement
 
+
+document.addEventListener('DOMContentLoaded', () => {
+    const cursor = document.querySelector('cursor') as HTMLElement;
+    const positionDisplay = document.getElementById('position') as HTMLElement;
+    document.addEventListener("mousemove",(e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+    cursor.style.left = x + "px";
+    cursor.style.top = y + "px";
+    positionDisplay.textContent = `${e.pageX}px, ${e.pageY}px`;
+    });
+})
+
 increase.addEventListener("click",() =>{
     counter++;
     console.log(counter);
