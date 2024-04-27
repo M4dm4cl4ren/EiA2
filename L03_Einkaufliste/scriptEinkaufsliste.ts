7b94bd9e44ff5876312b066011760616b49a5a00
Marc Dressler <madmac.123456789@gmail.com>
2024-04-28 01:11:41 +0200

    morgen weiter machen

--- L03_Einkaufliste/scriptEinkaufsliste.ts
+++ L03_Einkaufliste/scriptEinkaufsliste.ts
@@ -1,3 +1,51 @@
 import * as fs from "fs";
 
-document.getElementById("Abchlussbutton").addEventListener(onclick, produkteEntfernen) as HTMLElement;
+const jsonPath = "dataEinkaufsliste.json"
+const produkt = document.getElementById("produkt") as HTMLInputElement;
+const menge = document.getElementById("menge") as  HTMLInputElement;
+const marke = document.getElementById('marke') as HTMLInputElement;
+
+
+function jsonRead(): any {
+    try {
+        const data = fs.readFileSync(jsonPath, 'utf8');
+        return JSON.parse(data);
+    } catch (error) {
+        console.error("Es gab ein Fehler", error);
+        return null;
+    }
+}
+
+function jsonWrite(data: any): void {
+    try {
+        fs.writeFileSync(jsonPath, JSON.stringify(data, null, 4));
+        console.log("Daten wurden gespeichert");
+    } catch (error) {
+        console.log("Es gab einen Fehler");
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("Hinzufügen")?.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const newData = {
+            produkt: produkt.value,
+            menge: parseInt(menge.value),
+            marke: marke.value
+        };
+
+        const existingData = jsonRead();
+        if (existingData !== null) {
+            existingData.push(newData);
+            jsonWrite(existingData);
+
+            produkt.value = "";
+            menge.value = "";
+            marke.value = "";
+            alert("Neues Produkt hinzugefügt!");
+        } else {
+            alert("Es gab ein Problem beim Lesen der Datei.");
+        }
+    });
+});
